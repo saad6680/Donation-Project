9a20e84730203aa8783b31fb1c573bf504f87629
JohnIhab <elmalomawy@gmail.com>
2025-05-12 21:57:52 +0300

    make home page

--- public/main.js
+++ public/main.js
@@ -1,5 +1,21 @@
+// add navbar in index.html
 fetch('./Navbar/navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-placeholder').innerHTML = data;
     });
+
+// add footer in index.html
+fetch('./Footer/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    });
+
+// add Home in index.html
+fetch('./Home/home.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('home-placeholder').innerHTML = data;
+    });
+
